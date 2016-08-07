88bbe21595dd14d4d85ce2d369825428f3c7579d
Alan Fernandez <Alanf718@gmail.com>
2016-08-07 17:10:03 -0400

    Merge branch 'master' of https://github.com/Alanf718/alnick-project1 into alanf718/enemies
    # Conflicts:
    #	src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,57 @@
-/**
- * Created by Alan on 8/7/2016.
- */
+var enemies = require('./enemies.js');
 
-var x = "Hey nick remember to remove me";
-alert(x);
+var x = 500;
+var y = 900;
+var numPixels = 10;
+
+function setup() {
+    createCanvas(1000, 1000);
+    enemies.init();
+}
+
+function userInputs(){
+    if (keyIsDown(65))
+        x-=5;
+
+    if (keyIsDown(68))
+        x+=5;
+
+    if (keyIsDown(87))
+        y-=5;
+
+    if (keyIsDown(83))
+        y+=5;
+};
+
+function update() {
+    enemies.updateEnemies();
+};
+
+
+function draw() {
+
+    userInputs();
+
+    background(211, 211, 211);
+    fill(255, 0, 0);
+    ellipse(x, y, 50, 50);
+
+    // Enemy logic below
+    var nme = enemies.getEnemies();
+
+    update();
+
+    for(var i=0; i < nme.length; i++)
+    {
+        var e = nme[i];
+        if(e.black) {
+            fill(255, 0, 0);
+        } else {
+            fill(0, 0, 255);
+        }
+        ellipse(e.x, e.y, 35, 35);
+    }
+}
+
+global.setup = setup;
+global.draw = draw;
