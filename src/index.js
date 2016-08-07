224a5ff43fbf6859c9ac39bd3c82d130e5a2f0fb
Alan Fernandez <Alanf718@gmail.com>
2016-08-07 17:37:05 -0400

    Merge branch 'master' of https://github.com/Alanf718/alnick-project1 into alanf718/refactoring
    # Conflicts:
    #	src/index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ var enemies = require('./enemies.js');
 
 var x = 500;
 var y = 900;
+
+var heroWidth = 50;
+var minWidth = 10;
+var maxWidth = 75;
+
 var numPixels = 10;
+
+var vel = 5;
 
 function setup() {
     createCanvas(1000, 1000);
@@ -11,22 +18,36 @@ function setup() {
 
 function userInputs(){
     if (keyIsDown(65))
-        x-=5;
+        x-=vel;
 
     if (keyIsDown(68))
-        x+=5;
+        x+=vel;
 
     if (keyIsDown(87))
-        y-=5;
+        y-=vel;
 
     if (keyIsDown(83))
-        y+=5;
+        y+=vel;
+
+    if (keyIsDown(LEFT_ARROW)){
+        if(heroWidth >= minWidth + .4){
+            heroWidth -= .4;
+            vel = heroWidth/10;
+        } else {
+        }
+    }
+    if (keyIsDown(RIGHT_ARROW)){
+        if(heroWidth <= maxWidth - .4){
+            heroWidth += .4;
+            vel = heroWidth/10;
+        } else {
+        }
+    }
 };
 
 function update() {
     enemies.updateEnemies();
 };
-
 
 function draw() {
 
@@ -34,7 +55,7 @@ function draw() {
 
     background(211, 211, 211);
     fill(255, 0, 0);
-    ellipse(x, y, 50, 50);
+    ellipse(x, y, heroWidth, 50);
 
     // Enemy logic below
     var nme = enemies.getEnemies();
