6a031adb67fd6a165226ab2305ab141bb286723b
Nicholas Llerandi <nicholasllerandi@gmail.com>
2016-08-07 16:14:19 -0400

    the little red guy is moving

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
+var xpos = 500;
+
+var ypos = 900;
+
+var numPixels = 10;
+
 function setup() {
     createCanvas(1000, 1000);
 }
 
 function draw() {
-    if (keyIsPressed === true) {
-        fill(0);
-    } else {
-        fill(255);
+    background(211,211,211);
+
+    fill(255, 0, 0);
+
+    stroke(0);
+
+    ellipseMode(CENTER);
+
+    ellipse(xpos, ypos, 25, 25);
+}
+
+function keyPressed() {
+    // UP key
+    if(keyCode == UP_ARROW)
+    {
+        ypos = ypos - numPixels;
     }
-    rect(width/2, 900, 50, 50);
+
+    // DOWN key
+    if(keyCode == DOWN_ARROW)
+    {
+        ypos = ypos + numPixels;
+    }
+
+    // RIGHT key
+    if(keyCode == RIGHT_ARROW)
+    {
+        xpos = xpos + numPixels;
+    }
+
+    // LEFT key
+    if(keyCode == LEFT_ARROW)
+    {
+        xpos = xpos - numPixels;
+    }
 }
