f1fe8c5a80740727f741b34d1257e6310c8c1b98
Nicholas Llerandi <nicholasllerandi@gmail.com>
2016-08-07 16:42:31 -0400

    using keyIsDown instead of keyPressed

--- src/index.js
+++ src/index.js
@@ -1,47 +1,24 @@
-var xpos = 500;
-
-var ypos = 900;
-
-var numPixels = 10;
+var x = 500;
+var y = 950;
 
 function setup() {
     createCanvas(1000, 1000);
 }
 
 function draw() {
-    background(211,211,211);
+    if (keyIsDown(65))
+        x-=5;
 
+    if (keyIsDown(68))
+        x+=5;
+
+    if (keyIsDown(87))
+        y-=5;
+
+    if (keyIsDown(83))
+        y+=5;
+
+    background(211, 211, 211);
     fill(255, 0, 0);
-
-    stroke(0);
-
-    ellipseMode(CENTER);
-
-    ellipse(xpos, ypos, 25, 25);
-}
-
-function keyPressed() {
-    // UP key
-    if(keyCode == UP_ARROW)
-    {
-        ypos = ypos - numPixels;
-    }
-
-    // DOWN key
-    if(keyCode == DOWN_ARROW)
-    {
-        ypos = ypos + numPixels;
-    }
-
-    // RIGHT key
-    if(keyCode == RIGHT_ARROW)
-    {
-        xpos = xpos + numPixels;
-    }
-
-    // LEFT key
-    if(keyCode == LEFT_ARROW)
-    {
-        xpos = xpos - numPixels;
-    }
+    ellipse(x, y, 50, 50);
 }
